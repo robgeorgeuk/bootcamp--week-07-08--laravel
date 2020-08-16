8ce3287128033cd180fc994eb4c26311e93279e8
Rob George <rob@robgeorge.co.uk>
2020-08-16 14:31:29 +0100

    correct property name in ArticleResource

--- notes/15-validation-resources/figures/04-resource.php
+++ notes/15-validation-resources/figures/04-resource.php
@@ -2,11 +2,11 @@
 
 public function toArray($request)
 {
-  // just show the id, title, and article properties
+  // just show the id, title, and content properties
   // $this represents the current article
   return [
     "id" => $this->id,
     "title" => $this->title,
-    "article" => $this->article,
+    "content" => $this->content,
   ];
 }
