49d416d65249a0c0064a3c26577adcea41e6da72
Rob George <rob@robgeorge.co.uk>
2020-11-09 12:31:48 +0000

    Update blade loop figure to prevent errors
    Reference Articles from Models and use updates blade syntax for comments.

--- notes/07-blade/figures/06-loop.blade.php
+++ notes/07-blade/figures/06-loop.blade.php
@@ -1,24 +1,22 @@
-@extends("app")
-
 @section("content")
   <div class="list-group">
 
-    { /* loop over all of the articles */ }
-    { /* each article object goes into $article */ }
-    @foreach (App\Article::all() as $article)
-      <a href="/articles/{{ $article->id }}" class="list-group-item list-group-item-action">
-        <div class="d-flex w-100 justify-content-between">
+      {{-- loop over all of the articles --}}
+      {{-- each article object goes into $article --}}
+      @foreach (App\Models\Article::all() as $article)
+        <a href="/articles/{{ $article->id }}" class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
 
-          { /* output the article title */ }
-          <h5 class="mb-1">{{ $article->title }}</h5>
+                {{-- output the article title --}}
+                <h5 class="mb-1">{{ $article->title }}</h5>
 
-          { /* use the relativeDate() method */ }
-          <small>{{ $article->relativeDate() }}</small>
-        </div>
+                {{-- use the relativeDate() method --}}
+                <small>{{ $article->relativeDate() }}</small>
+            </div>
 
-        { /* output the truncated content */ }
-        <p class="mb-1">{{ $article->truncate() }}</p>
-      </a>
-    @endforeach
+            {{-- output the truncated content --}}
+            <p class="mb-1">{{ $article->truncate() }}</p>
+        </a>
+      @endforeach
   </div>
 @endsection
